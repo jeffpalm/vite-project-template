37fa42e37c7647124d30edf196f4f7a35d5c75ca
Jeff Palmer <jeffp@covr.care>
2022-02-11 16:11:56 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './reset.css'
+import styled from 'styled-components'
+
+const Root = styled.div`
+  img {
+    width: 100px;
+  }
+
+  p {
+
+  }
+`
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Root>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </Root>
+  )
+}
+
+export default App
